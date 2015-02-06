a563f49844779d9f66c0f5333f4623675a712690
luis <luisbigvargas@gmail.com>
2015-02-05 23:43:21 -0500

    test sample heroku dart

--- bin/server.dart
+++ bin/server.dart
@@ -1,48 +1,69 @@
-library drails_example;
+//library drails_example;
+//
+//import 'package:drails/drails.dart';
+//import 'package:logging/logging.dart';
+//import 'dart:io';
+//import 'package:drails_sample_app/models.dart';
+//
+//part 'controllers/persons_controller.dart';
+//part 'controllers/employee_controller.dart';
+//part 'data/persons_data.dart';
+//
+//
+//initLogger() {
+//  Logger.root.level = Level.OFF;
+//  hierarchicalLoggingEnabled = true;
+//  new Logger('server_init').level = Level.INFO;
+//
+//  Logger.root.onRecord.listen((LogRecord rec) {
+//    print('${rec.level.name}: ${rec.time}: ${rec.message}');
+//  });
+//}
+//
+//void main() {
+//  initLogger();
+//
+//  ENV = 'prod';
+//  CLIENT_DIR['prod'] = '/../web/';
+//
+//  POST['/login'] = (HttpSession session, @RequestBody User user) {
+//    var cu = g_users.values.singleWhere((u) => u.name == user.name && u.password == user.password);
+//    if(cu == null) throw Exception;
+//    session['user'] = cu;
+//    return new User()
+//        ..name = cu.name
+//        ..roles = cu.roles;
+//  };
+//
+//  GET['/logout'] = (HttpSession session) {
+//    session['user'] = null;
+//  };
+//  var portEnv = Platform.environment['PORT'];
+//  var port = portEnv == null ? 4040 : int.parse(portEnv);
+//
+//  print('portEnv: $portEnv');
+//  print('port: $port');
+//
+//  initServer([#drails_example], port: port);
+//}
 
-import 'package:drails/drails.dart';
-import 'package:logging/logging.dart';
-import 'dart:io';
-import 'package:drails_sample_app/models.dart';
-
-part 'controllers/persons_controller.dart';
-part 'controllers/employee_controller.dart';
-part 'data/persons_data.dart';
-
-
-initLogger() {
-  Logger.root.level = Level.OFF;
-  hierarchicalLoggingEnabled = true;
-  new Logger('server_init').level = Level.INFO;
-  
-  Logger.root.onRecord.listen((LogRecord rec) {
-    print('${rec.level.name}: ${rec.time}: ${rec.message}');
-  });
-}
+import 'dart:io' show Platform;
+import 'dart:async' show runZoned;
+import 'package:path/path.dart' show join, dirname;
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_static/shelf_static.dart';
 
 void main() {
-  initLogger();
+  // Assumes the server lives in bin/ and that `pub build` ran
+  var pathToBuild = join(dirname(Platform.script.toFilePath()), '..', 'build/web');
   
-  ENV = 'prod';
-  CLIENT_DIR['prod'] = '/../web/'; 
+  var handler = createStaticHandler(pathToBuild, defaultDocument: 'index.html');
   
-  POST['/login'] = (HttpSession session, @RequestBody User user) {
-    var cu = g_users.values.singleWhere((u) => u.name == user.name && u.password == user.password);
-    if(cu == null) throw Exception;
-    session['user'] = cu; 
-    return new User()
-        ..name = cu.name
-        ..roles = cu.roles;
-  };
-  
-  GET['/logout'] = (HttpSession session) {
-    session['user'] = null;
-  };
   var portEnv = Platform.environment['PORT'];
-  var port = portEnv == null ? 4040 : int.parse(portEnv);
+  var port = portEnv == null ? 9999 : int.parse(portEnv);
   
-  print('portEnv: $portEnv');
-  print('port: $port');
-  
-  initServer([#drails_example], port: port);
+  runZoned(() {
+    io.serve(handler, '0.0.0.0', port);
+    print("Serving $pathToBuild on port $port");
+  }, onError: (e, stackTrace) => print('Oh noes! $e $stackTrace'));
 }
